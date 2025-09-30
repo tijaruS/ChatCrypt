00ab44345ab1007ef49b4047474e6ef4d677e89f
tijaruS <surajitdalelpara25@gmail.com>
2025-09-30 18:28:31 +0530

    Initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,8 +19,8 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist/")));
-  app.get("*", () => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
